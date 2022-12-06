d886f760427233361d51fefa947f82a3d4063b4f
Ronald Crooy <r.a.crooy@gmail.com>
2022-12-06 10:06:43 +0100

    day 6

--- src/main/scala/crooy/FileListInput.scala
+++ src/main/scala/crooy/FileListInput.scala
@@ -1,18 +1,17 @@
 package crooy
 
 import zio.*
-  
+
 import scala.io.Source
 
 case class FileListInput(name: String, list: List[String])
 
 object FileListInput {
-  
 
   private def create(name: String) = ZIO.succeed {
-    val fileStream = getClass.getResourceAsStream(name)
+    val fileStream  = getClass.getResourceAsStream(name)
     val inputStream = Source.fromInputStream(fileStream)
-    val result = Source.fromInputStream(fileStream).getLines.toList
+    val result      = Source.fromInputStream(fileStream).getLines.toList
     inputStream.close()
     fileStream.close()
     FileListInput(name, result)
